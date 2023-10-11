7eae1cf327cee87d8047b5d8a8e16185fbfabce0
kelvin ward <canigetyournumber101010@gmail.com>
2023-10-11 19:50:33 +0700

    fix api

---
+++ app/api/posts/[postId]/route.ts
@@ -0,0 +1,42 @@
+import getCurrentUser from "@/app/actions/getCurrentUser";
+import { NextResponse } from "next/server";
+
+import prisma from "@/app/libs/prismadb";
+
+interface IParams {
+    postId: string
+}
+
+export async function GET(
+    request: Request,
+    { params }: { params: IParams }
+) {
+    try {
+        const currentUser = await getCurrentUser();
+
+        if (!currentUser?.email) {
+            return null;
+        }
+
+        const post = await prisma.post.findUnique({
+            where: {
+                id: params.postId
+            },
+            include: {
+                user: true,
+                comments: {
+                    include: {
+                        user: true
+                    },
+                    orderBy: {
+                        createdAt: 'desc'
+                    }
+                },
+            },
+        });
+
+        return NextResponse.json(post)
+    } catch (error) {
+        return NextResponse.json(null);
+    }
+}
--- app/api/posts/route.ts
+++ app/api/posts/route.ts
@@ -32,3 +32,54 @@ export async function POST(
         return new NextResponse('Error', { status: 500 });
     }
 }
+
+interface IParams {
+    userId?: string;
+}
+
+export async function GET(
+    request: Request,
+    { params }: { params: IParams }
+) {
+    try {
+
+        const currentUser = await getCurrentUser();
+        if (!currentUser?.id) {
+            return [];
+        }
+
+
+        if (!currentUser?.id || !currentUser?.email) {
+            return new NextResponse('Unauthorized', { status: 401 });
+        }
+
+        let posts;
+
+        if (currentUser?.id && typeof currentUser?.id === 'string' && params.userId) {
+            posts = await prisma.post.findMany({
+                where: {
+                    userId: currentUser?.id
+                },
+                include: {
+                    user: true,
+                    comments: true
+                },
+                orderBy: {
+                    createdAt: 'desc'
+                },
+            });
+        } else {
+            posts = await prisma.post.findMany({
+                orderBy: {
+                    createdAt: 'desc'
+                }
+            });
+        }
+        
+
+        return NextResponse.json(posts)
+    } catch (error) {
+        console.log(error, 'ERROR_MESSAGES')
+        return new NextResponse('Error', { status: 500 });
+    }
+}
